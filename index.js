67769d793dd1e66bf3f76ec2f18d43d3794357e7
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-29 21:15:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, arg) {
+  return fn.apply(thisValue, arg);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  
+}
