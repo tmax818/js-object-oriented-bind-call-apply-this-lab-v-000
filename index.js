ab1403feaf82e1de5798bb020710d99014af00b2
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-29 21:24:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, arg) {
+  return fn.apply(thisValue, arg);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  let fncopy = functionToBeCopied.bind(thisValue)
+  return fncopy;
+}
